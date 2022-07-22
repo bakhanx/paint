97a5cecd674cadfcd66fda8c070abd787995eee4
BaKhaN <46181173+bakhanx@users.noreply.github.com>
2022-07-22 21:01:28 +0900

    painting mode

--- app.js
+++ app.js
@@ -1,29 +1,43 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // context is like a brush
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const modeBtn = document.getElementById("fill-btn");
+const modeName = document.getElementById("mode-name");
+const clearBtn = document.getElementById("clear-btn");
+const EraserBtn = document.getElementById("eraser-btn");
+// ============= const
+const CANVAS_WIDTH = 720;
+const CANVAS_HEIGHT = 480;
 
 // ============= css
-canvas.width = 1080;
-canvas.height = 720;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 const colors = [
-    "#55efc4",
-    "#81ecec",
-    "#74b9ff",
-    "#a29bfe",
-    "#dfe6e9",
-    "#fdcb6e",
-    "#e17055",
-    "#d63031",
-    "#e84393",
-    "#2d3436",
-  ];
+  "#55efc4",
+  "#81ecec",
+  "#74b9ff",
+  "#a29bfe",
+  "#dfe6e9",
+  "#fdcb6e",
+  "#e17055",
+  "#d63031",
+  "#e84393",
+  "#2d3436",
+];
 
 let isPainting = false;
+let isFilling = false;
+let eraserColor = "";
 
 // ============ init
-lineWidth.value= 5;
+lineWidth.value = 5;
 ctx.lineWidth = lineWidth.value;
+color.value = "black";
 
 // ============ handle Function
 
@@ -37,34 +51,72 @@ const onMove = (event) => {
 };
 
 const startPainting = () => {
-    isPainting = true;
+  isPainting = true;
+
+  if (isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    eraserColor = ctx.fillStyle;
   };
+
+
+
+};
 const stopPainting = () => {
   isPainting = false;
   ctx.beginPath();
 };
 
-const onLineWidthChange = (event)=>{
-    ctx.lineWidth = event.currentTarget.value;
-}
+const onLineWidthChange = (event) => {
+  ctx.lineWidth = event.currentTarget.value;
+};
 
+const onColorChange = (event) => {
+  ctx.strokeStyle = event.currentTarget.value;
+  ctx.fillStyle = event.currentTarget.value;
+};
+
+const onColorClick = (event) => {
+  const currentColor = event.target.dataset.color;
+  ctx.strokeStyle = currentColor;
+  ctx.fillStyle = currentColor;
+  color.value = currentColor;
+};
+
+const onModeClick = () => {
+  isFilling = !isFilling;
+
+  isFilling
+    ? ((modeBtn.innerHTML = "Draw"), (modeName.innerHTML = "Mode : Fill"))
+    : ((modeBtn.innerHTML = "Fill"), (modeName.innerHTML = "Mode : Draw"));
+};
+
+const onClearClick = () => {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
+
+const onEraseClick = () => {
+  ctx.strokeStyle = eraserColor;
+  isFilling = false;
+  modeBtn.innerHTML = "Fill";
+
+  console.log(canvas);
+};
 // ============= Event Listner
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", stopPainting);
 canvas.addEventListener("mouseleave", stopPainting);
-lineWidth.addEventListener("click",onLineWidthChange);
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => {
+  color.addEventListener("click", onColorClick);
+});
 
-
-
-
-
-
-
-
-
-
+modeBtn.addEventListener("click", onModeClick);
+clearBtn.addEventListener("click", onClearClick);
+EraserBtn.addEventListener("click", onEraseClick);
 const startTopLeft = (event) => {
   ctx.beginPath();
   ctx.moveTo(0, 0);
@@ -112,7 +164,6 @@ const attackEyes = (event) => {
 // ctx.lineTo(450,200);
 // ctx.stroke();
 // ctx.fill();
-
 
 // ============== Man
 
